7c582118248a061d1035b46dcd783e4a411a9dd6
Serkan Şahin <serkana607@gmail.com>
2022-04-17 15:29:35 +0300

    Varlıklar Oluşturuldu

---
+++ BlogSite/BlogSite.Entities/Concrete/Comment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogSite.Entities.Concrete
+{
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+        public string CommentUserName { get; set; }
+        public string Content { get; set; }
+        public string CommentTitle { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public bool Status { get; set; }
+    }
+}
